388e74a3037a5275484bbe886226c1a5fc409d87
gribkovvm <gribkovvm@solit.by>
2021-11-09 09:57:09 +0300

    refactor: stream atbash

--- Streams/TransformerAtbash.js
+++ Streams/TransformerAtbash.js
@@ -2,7 +2,7 @@ const Transformer = require('./Transformer');
 
 class TransformerAtbash extends Transformer {
     constructor() {
-        super(25, 1, true);
+        super(0, 1, true);
     }
 }
 module.exports = TransformerAtbash;
